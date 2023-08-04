3edc6452f21abdc739605f2101e3567ec03c7f86
David <da380@cam.ac.uk>
2023-08-04 16:19:54 +0100

    work on cubic splines

--- Interp/src/CubicSpline.h
+++ Interp/src/CubicSpline.h
@@ -1,16 +1,46 @@
 #ifndef INTERP_CUBIC_SPLINE_GUARD_H
 #define INTERP_CUBIC_SPLINE_GUARD_H
 
+#include <Eigen/Core>
+#include <iostream>
+#include <vector>
+
 namespace Interp {
 
-template <typename OrdIter, typename CordIter>
+template <typename xIter, typename yIter>
 class CubicSpline {
  public:
-  using ordinate_value_type = OrdIter::value_type;
-  using coordinate_value_type = CordIter::value_type;
+  // Define some class member types
+  using x_value_type = xIter::value_type;
+  using y_value_type = yIter::value_type;
+
+  // Declare the constructor.
+  CubicSpline(xIter, xIter, yIter);
+
+  // Declare the application operator.
+  y_value_type operator()(x_value_type);
 
  private:
+  using xVector = Eigen::Matrix<x_value_type, Eigen::Dynamic, 1>;
+  using yVector = Eigen::Matrix<y_value_type, Eigen::Dynamic, 1>;
+
+  // Store iterators to the function data.
+  xIter xStart, xFinish;
+  yIter yStart;
 };
+
+template <typename xIter, typename yIter>
+CubicSpline<xIter, yIter>::CubicSpline(xIter xStart, xIter XFinish,
+                                       yIter yStart)
+    : xStart{xStart}, xFinish{xFinish}, yStart{yStart} {
+  std::cout << "Hello\n";
+}
+
+template <typename xIter, typename yIter>
+CubicSpline<xIter, yIter>::y_value_type CubicSpline<xIter, yIter>::operator()(
+    x_value_type x) {
+  return 0;
+}
 
 }  // namespace Interp
 
